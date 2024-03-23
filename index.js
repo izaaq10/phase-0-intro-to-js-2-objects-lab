804ea85d850385c8e01a80bd959ef107bf169b36
Izaaq <odongoizaaq@gmail.com>
2024-03-23 09:37:19 -0400

    Adds object

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+    name : "Izaaq",
+    streetAddress: "tokyo123",
+}
+function updateEmployeeWithKeyAndValue(obj, key, value){
+const newObj = { ...obj };
+newObj[key] = value;
+
+  return newObj;
+}
+for (const key in employee) {
+    delete employee["Izaaq"];
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    const newObj = { ...obj };
+    newObj.streetAddress = "12 Broadway";
+    return newObj;
+    
+}
+for (const key in employee){
+     employee["12 Broadway"];
+}
